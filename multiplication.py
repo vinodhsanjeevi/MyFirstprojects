e74e43cb023457280c76672f4f9b08ebf098ed5a
vinodhsanjeevi <57708813+vinodhsanjeevi@users.noreply.github.com>
2023-09-09 00:49:17 +0530

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -6,4 +6,7 @@ print(a)
 print(b)
 print(c)
 print("its is testing for multiplication file in vs code")
-
+print("take two numbers here, a,b")
+print("a value here")
+print("b value is here")
+print("final line from remote ----------")
