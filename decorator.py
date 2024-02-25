5570ac22edbc75475045c82c4416486235658626
vinodhsanjeevi <vin.sanjeevi@gmail.com>
2024-02-25 12:49:09 +0530

    adding decorator file to the existing code base

---
+++ decorator.py
@@ -0,0 +1,22 @@
+# Example program for decorartor in python
+def mynewdecor(func):
+    def inner(name):
+        if name == "vinodh":
+            print("hi, its good morning.")
+        elif name == "abc":
+            print("its characters only...")
+        elif name == "cow":
+            print("its animal.")
+        else:
+            print(func(name))
+    return inner
+
+
+@mynewdecor
+def take(name):
+    print("hi", name, 'good morning ...')
+
+
+take("vinodh")
+take("abc")
+take("cow")
